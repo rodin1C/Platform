5db9e97d01df78ba555117019c1bc6d47cb013af
rodin1c <rodin1c@gmail.com>
2016-09-13 19:29:06 +0300

    test

--- web-client/src/main/java/com/poker/platform/filter/AuthFilter.java
+++ web-client/src/main/java/com/poker/platform/filter/AuthFilter.java
@@ -2,6 +2,7 @@ package com.poker.platform.filter;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.util.Map;
 
 
 public class AuthFilter implements Filter {
@@ -12,7 +13,8 @@ public class AuthFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
+        Map<String, String[]> parameterMap = servletRequest.getParameterMap();
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
